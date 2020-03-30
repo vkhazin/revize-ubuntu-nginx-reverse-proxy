ff8d4b147f93d841472fe2e2cd68a2ed5924da1b
Vlad Khazin <vladimir.khazin@icssolutions.ca>
2020-03-30 16:18:15 -0400

    adding tcp proxy

--- readme.md
+++ readme.md
@@ -8,3 +8,7 @@
 * Clone the repository: `git clone https://github.com/vkhazin/revize-ubuntu-nginx-reverse-proxy.git`
 * Run the setup: `cd ./revize-ubuntu-nginx-reverse-proxy && chmod +x ./setup.sh && ./setup.sh`
 * Navigate to the url listed at the end of the script using a web browser, e.g.: http://1.1.1.1
+
+## References
+
+* https://serversforhackers.com/c/tcp-load-balancing-with-nginx-ssl-pass-thru
--- setup.sh
+++ setup.sh
@@ -2,9 +2,11 @@ set -e
 sudo apt-get update
 sudo apt-get install nginx -y
 sudo systemctl enable nginx
-sudo unlink /etc/nginx/sites-enabled/default || true
-sudo cp ./google.conf /etc/nginx/sites-available/revize.conf
-sudo ln -s /etc/nginx/sites-available/revize.conf /etc/nginx/sites-enabled/revize.conf
+sudo unlink /etc/nginx/sites-enabled/default | true
+sudo mkdir -p /etc/nginx/tcpconf.d/
+sudo cp ./revize-tcp.conf /etc/nginx/tcpconf.d/revize.conf
+sudo echo "# Passthrough tcp proxy" | tee -a /etc/nginx/nginx.conf
+sudo echo "include /etc/nginx/tcpconf.d/*;" | tee -a /etc/nginx/nginx.conf
 sudo systemctl reload nginx
-sduo cat /etc/nginx/sites-enabled/revize.conf
-echo "Open your browser to http://$(curl -s http://ipinfo.io/ip)"
+sudo cat /etc/nginx/sites-enabled/revize.conf
+echo "Open your browser to https://$(curl -s http://ipinfo.io/ip)"
