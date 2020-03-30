676664d6ed5117c836799da9c6d1f3b49e06c598
Vlad Khazin <vladimir.khazin@icssolutions.ca>
2020-03-30 16:20:12 -0400

    adding sudo before tee

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ sudo systemctl enable nginx
 sudo unlink /etc/nginx/sites-enabled/default | true
 sudo mkdir -p /etc/nginx/tcpconf.d/
 sudo cp ./revize-tcp.conf /etc/nginx/tcpconf.d/revize.conf
-sudo echo "# Passthrough tcp proxy" | tee -a /etc/nginx/nginx.conf
-sudo echo "include /etc/nginx/tcpconf.d/*;" | tee -a /etc/nginx/nginx.conf
+echo "# Passthrough tcp proxy" | sudo tee -a /etc/nginx/nginx.conf
+echo "include /etc/nginx/tcpconf.d/*;" | sudo tee -a /etc/nginx/nginx.conf
 sudo systemctl reload nginx
 sudo cat /etc/nginx/sites-enabled/revize.conf
 echo "Open your browser to https://$(curl -s http://ipinfo.io/ip)"
