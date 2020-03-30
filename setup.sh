1def7011ce8843594dbe8066d7b6c0b6804555d6
Vlad Khazin <vladimir.khazin@icssolutions.ca>
2020-03-30 14:58:32 -0400

    update setup

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@ sudo apt-get update
 sudo apt-get install nginx -y
 sudo systemctl enable nginx
 sudo unlink /etc/nginx/sites-enabled/default
-sudo cat ./google.conf | sudo tee /etc/nginx/sites-available/revize.conf
+sudo cp ./google.conf /etc/nginx/sites-available/revize.conf
 sudo ln -s /etc/nginx/sites-available/revize.conf /etc/nginx/sites-enabled/revize.conf
 sudo systemctl reload nginx
+sduo cat /etc/nginx/sites-enabled/revize.conf
 echo "Open your browser to http://$(curl -s http://ipinfo.io/ip)"
