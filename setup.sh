fa2d3bfa165b3e0ae0dc7a1db40eabdaf56d8db1
Vlad Khazin <vladimir.khazin@icssolutions.ca>
2020-03-30 14:29:17 -0400

    swtich from google to revize

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get install nginx -y
 sudo systemctl enable nginx
 sudo unlink /etc/nginx/sites-enabled/default
-sudo cat ./google.conf | sudo tee /etc/nginx/sites-available/google-reverse-proxy.conf
-sudo ln -s /etc/nginx/sites-available/google-reverse-proxy.conf /etc/nginx/sites-enabled/google-reverse-proxy.conf
+sudo cat ./google.conf | sudo tee /etc/nginx/sites-available/revize.conf
+sudo ln -s /etc/nginx/sites-available/revize.conf /etc/nginx/sites-enabled/revize.conf
 sudo systemctl reload nginx
 echo "Open your browser to http://$(curl -s http://ipinfo.io/ip)"
