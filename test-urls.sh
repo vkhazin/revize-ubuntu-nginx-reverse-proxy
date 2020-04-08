b26c672c66813ddb80fe5089cb75af56de627548
Vlad Khazin <vladimir.khazin@icssolutions.ca>
2020-04-08 08:10:08 -0400

    http enabled

---
+++ test-urls.sh
@@ -0,0 +1,8 @@
+curl 'https://revize.icssolutions.ca'
+curl 'http://revize.icssolutions.ca'
+
+# curl 'https://revize1.icssolutions.ca'
+# curl 'http://revize1.icssolutions.ca'
+
+# curl 'https://revize3.icssolutions.ca'
+# curl 'http://revize3.icssolutions.ca'
